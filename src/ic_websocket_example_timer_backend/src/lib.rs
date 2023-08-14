81e5f4924b7959194712ad32db57ef443c0ee5e4
Luca8991 <luca.berte98@hotmail.it>
2023-08-14 18:50:14 +0200

    fix: use remote gateway

--- src/ic_websocket_example_timer_backend/src/lib.rs
+++ src/ic_websocket_example_timer_backend/src/lib.rs
@@ -11,10 +11,10 @@ use ic_websocket_cdk::{
 mod canister;
 
 /// This is the principal of the WS Gateway deployed on wss://icws.io
-// pub const GATEWAY_PRINCIPAL: &str =
-//     "3656s-3kqlj-dkm5d-oputg-ymybu-4gnuq-7aojd-w2fzw-5lfp2-4zhx3-4ae";
 pub const GATEWAY_PRINCIPAL: &str =
-    "sqdfl-mr4km-2hfjy-gajqo-xqvh7-hf4mf-nra4i-3it6l-neaw4-soolw-tae";
+    "3656s-3kqlj-dkm5d-oputg-ymybu-4gnuq-7aojd-w2fzw-5lfp2-4zhx3-4ae";
+// pub const GATEWAY_PRINCIPAL: &str =
+//     "sqdfl-mr4km-2hfjy-gajqo-xqvh7-hf4mf-nra4i-3it6l-neaw4-soolw-tae";
 
 #[init]
 fn init() {
