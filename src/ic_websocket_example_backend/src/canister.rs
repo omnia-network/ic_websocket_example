e541f57aa0069ca7c91f3ac47ee72e8e1951dbe7
Luca8991 <luca.berte98@hotmail.it>
2023-12-15 22:59:02 +0100

    chore: update sdks

--- src/ic_websocket_example_backend/src/canister.rs
+++ src/ic_websocket_example_backend/src/canister.rs
@@ -1,9 +1,9 @@
-use candid::{CandidType, encode_one, decode_one};
-use ic_cdk::{print, api::time};
+use candid::{decode_one, encode_one, CandidType};
+use ic_cdk::{api::time, print};
 use serde::{Deserialize, Serialize};
 
 use ic_websocket_cdk::{
-    ws_send, ClientPrincipal, OnCloseCallbackArgs, OnMessageCallbackArgs, OnOpenCallbackArgs,
+    send, ClientPrincipal, OnCloseCallbackArgs, OnMessageCallbackArgs, OnOpenCallbackArgs,
 };
 
 #[derive(CandidType, Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
@@ -38,7 +38,7 @@ pub fn on_message(args: OnMessageCallbackArgs) {
 
 fn send_app_message(client_principal: ClientPrincipal, msg: AppMessage) {
     print(format!("Sending message: {:?}", msg));
-    if let Err(e) = ws_send(client_principal, msg.candid_serialize()) {
+    if let Err(e) = send(client_principal, msg.candid_serialize()) {
         println!("Could not send message: {}", e);
     }
 }
